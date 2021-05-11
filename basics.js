6a9064ab4f9880d3260542b55421cc5610f4634c
Sachi Tripathi <sachitripathi25@gmail.com>
2021-05-11 19:14:24 +0530

    added code

---
+++ basics.js
@@ -0,0 +1,11 @@
+/*DOM-Document Object Model is basically an API(Application Programming Interface) which lets two applications talk to one another. It
+ is a tree-like structure which creates HTML file more interactive. It has nodes, elements, attributes etc.*/
+ 
+ /*JS does'nt have any buitl-in print function. It has other statements for printing like
+ an alert box using window.alert()
+ a prompt using window.prompt()
+ HTML output using document.write()
+ HTML element using innerHTML()
+ the browser console using console.log() */
+
+ 
